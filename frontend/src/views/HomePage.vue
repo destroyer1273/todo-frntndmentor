8b7d190d5d5ce8c196471d5fd408245417e1253b
matvei_oskin <rofl.1991@list.ru>
2026-02-01 11:41:19 +0700

    shapka dobavlena

---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="background">
+    <div class="main">
+        <div class="main__header">
+          <div class="main__header__cont1">
+            <h1>TODO</h1>
+            <img src="/images/icon-sun.svg" alt="Солнышко">
+          </div>
+          <form>
+            <input type="text" placeholder="Create a new todo...">
+          </form>
+        </div>
+        <div class="main_bottom">
+
+        </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.background {
+    width: 100%;
+    height: 100%;
+    background-image: url("/images/bg-desktop-dark.jpg");
+    background-repeat: no-repeat;
+    padding-top: 50px;
+}
+.main {
+    max-width: 500px;
+    margin: 0 auto;
+}
+.main__header__cont1 {
+    display: flex;
+    flex-direction: row;
+}
+.main__header__cont1 h1 {
+    color: #FFF;
+}
+</style>
